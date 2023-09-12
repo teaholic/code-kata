0ad054b6311a2d49684d26c2901d033d99e4afbf
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2023-09-12 16:46:11 +0200

    Refactoring tests to help debugging random failures

--- tests/tennis/test_main.py
+++ tests/tennis/test_main.py
@@ -1,4 +1,7 @@
 from unittest import TestCase
+
+from parameterized import parameterized
+
 from tennis.main import (
     Dashboard,
     Umpire,
@@ -9,27 +12,35 @@ from tennis.main import (
 
 
 class TestDashboard(TestCase):
-    def test_update(self):
-        test_cases = [
-            [["1", "1", "2", "1"], Score(player1=Point(3), player2=Point(1))],
-            [["2", "1", "1", "2", "2"], Score(player1=Point(2), player2=Point(3))],
-            [["2", "1", "2", "1", "2", "1"], Score(player1=Point(4), player2=Point(4))],
+    @parameterized.expand(
+        [
+            [["1", "1", "2", "1"], Score(player1=Point.FORTY, player2=Point.FIFTEEN)],
+            [
+                ["2", "1", "1", "2", "2"],
+                Score(player1=Point.THIRTY, player2=Point.FORTY),
+            ],
+            [
+                ["2", "1", "2", "1", "2", "1"],
+                Score(player1=Point.DEUCE, player2=Point.DEUCE),
+            ],
             [
                 ["2", "1", "2", "1", "2", "1", "1"],
-                Score(player1=Point(5), player2=Point(4)),
+                Score(player1=Point.ADVANTAGE, player2=Point.DEUCE),
             ],
         ]
-
-        for game, expected in test_cases:
-            dashboard = Dashboard(game)
-            actual = dashboard.update()
-            self.assertEqual(actual.player1.value, expected.player1.value)
-            self.assertEqual(actual.player2.value, expected.player2.value)
+    )
+    def test_update(self, game, expected):
+        dashboard = Dashboard(game)
+        actual = dashboard.update()
+        self.assertEqual(actual.player1.name, expected.player1.name)
+        self.assertEqual(actual.player2.name, expected.player2.name)
 
 
 class TestUmpire(TestCase):
-    def test_find_winner(self):
-        test_cases = [
+    umpire = Umpire()
+
+    @parameterized.expand(
+        [
             [Score(player1=Point.FORTY, player2=Point.FIFTEEN), "Player 1"],
             [Score(player1=Point.LOVE, player2=Point.FORTY), "Player 2"],
             [Score(player1=Point.ADVANTAGE, player2=Point.THIRTY), "Player 1"],
@@ -39,16 +50,15 @@ class TestUmpire(TestCase):
             [Score(player1=Point.FORTY, player2=Point.FORTY), "invalid game"],
             [Score(player1=Point.DEUCE, player2=Point.DEUCE), "invalid game"],
         ]
-
-        umpire = Umpire()
-        for scores, expected in test_cases:
-            actual = umpire.find_winner(scores, ["Player 1", "Player 2"])
-            self.assertEqual(actual, expected)
+    )
+    def test_find_winner(self, scores, expected):
+        actual = self.umpire.find_winner(scores, ["Player 1", "Player 2"])
+        self.assertEqual(actual, expected)
 
 
 class TestTennisGame(TestCase):
-    def test_run(self):
-        test_cases = [
+    @parameterized.expand(
+        [
             [["1", "1", "1", "2"], "1"],
             [["2", "1", "1", "1"], "1"],
             [["2", "1", "1", "2", "2", "2", "2"], "2"],
@@ -56,8 +66,8 @@ class TestTennisGame(TestCase):
             [["a", "b", "a"], "invalid game"],
             [["2", "1", "1", "2", "1", "1"], "invalid game"],
         ]
-
-        for game, expected in test_cases:
-            engine = TennisGame(game)
-            actual = engine.run(game)
-            self.assertEqual(actual, expected)
+    )
+    def test_run(self, game, expected):
+        engine = TennisGame(game)
+        actual = engine.run(game)
+        self.assertEqual(actual, expected)
