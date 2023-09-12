c2848e78990be1023291b68ecf6024790460b3fe
Chiara Barbagianni <teaholic@users.noreply.github.com>
2023-09-12 13:18:19 +0200

    Added named tennis points (#7)
    Co-authored-by: Chiara Barbagianni <chiara.barbagianni@agilelab.it>

--- src/tennis/main.py
+++ src/tennis/main.py
@@ -1,24 +1,60 @@
+from enum import Enum
+from typing import NamedTuple, List
+
+
+class Point(Enum):
+    LOVE = 0
+    FIFTEEN = 1
+    THIRTY = 2
+    FORTY = 3
+    WIN = 4
+
+
+class PointService:
+    def update(self, point: Point):
+        return Point(point.value + 1)
+
+
+class Score(NamedTuple):
+    player1: Point
+    player2: Point
+
+    def get_top_scorer(self):
+        if self.player1.value > self.player2.value:
+            return 0
+        else:
+            return 1
+
+    def get_least_scorer(self):
+        if self.player1.value < self.player2.value:
+            return 0
+        else:
+            return 1
+
+
 class Dashboard:
     def __init__(self, game):
         self.game = game
-        self.player1, self.player2 = set(game)
-        self.scores = {self.player1: 0, self.player2: 0}
+        [self.player1, self.player2] = set(game)
+        self.scores = {self.player1: Point(0), self.player2: Point(0)}
+        self.service = PointService()
 
-    def update(self):
+    def update(self) -> Score:
         for match_id in range(len(self.game)):
             match_winner = self.game[match_id]
-            self.scores[match_winner] += 1
+            self.scores[match_winner] = self.service.update(self.scores[match_winner])
+        return Score(self.scores[self.player1], self.scores[self.player2])
 
 
 class Umpire:
-    def find_winner(self, scores: dict) -> str:
-        top_scorer = max(scores, key=scores.get)
-        opponent = min(scores, key=scores.get)
-        if scores[top_scorer] >= 4:
-            print("top_scorer points > 4")
-            if scores[top_scorer] - scores[opponent] >= 2:
+    def find_winner(self, scores: Score, players: List[str]) -> str:
+        top_scorer = scores.get_top_scorer()
+        opponent = scores.get_least_scorer()
+        if scores[top_scorer].value == 4:
+            print("top_scorer has 4 points")
+            if scores[top_scorer].value - scores[opponent].value >= 2:
                 print("top_scorer scored at least 2 points more than opponent")
-                return top_scorer
+                return players[top_scorer]
             else:
                 return "invalid game"
         else:
@@ -31,5 +67,6 @@ class TennisGame:
         self.umpire = Umpire()
 
     def run(self, game) -> str:
-        self.dashboard.update()
-        return self.umpire.find_winner(self.dashboard.scores)
+        return self.umpire.find_winner(
+            scores=self.dashboard.update(), players=list(set(game))
+        )
--- tests/tennis/test_main.py
+++ tests/tennis/test_main.py
@@ -1,34 +1,65 @@
 from unittest import TestCase
-from tennis.main import Dashboard, Umpire, TennisGame
+from tennis.main import Dashboard, Umpire, TennisGame, Point, PointService, Score
+
+
+class TestScoreService(TestCase):
+    def test_update(self):
+        test_cases = [
+            [Point(0), Point(1)],
+            [Point(1), Point(2)],
+            [Point(2), Point(3)],
+            [Point(3), Point(4)],
+        ]
+
+        service = PointService()
+        for point, expected in test_cases:
+            actual = service.update(point)
+            self.assertEqual(actual, expected)
+
+
+class TestScore(TestCase):
+    def test_get_top_scorer(self):
+        test_cases = [
+            [Score(Point(0), Point(2)), 1],
+            [Score(Point(3), Point(1)), 0],
+        ]
+
+        for score, expected in test_cases:
+            actual = score.get_top_scorer()
+            self.assertEqual(actual, expected)
 
 
 class TestDashboard(TestCase):
     def test_update(self):
         test_cases = [
-            [["1", "1", "1", "2", "1"], {"1": 4, "2": 1}],
-            [["2", "1", "1", "2", "2", "1", "2", "2"], {"1": 3, "2": 5}],
-            [["2", "1", "1", "2", "2", "1", "2", "1", "1", "1"], {"1": 6, "2": 4}],
+            [["1", "1", "2", "1"], Score(player1=Point(3), player2=Point(1))],
+            [["2", "1", "1", "2", "2"], Score(player1=Point(2), player2=Point(3))],
+            [["2", "1", "2", "1", "2", "1"], Score(player1=Point(3), player2=Point(3))],
+            [
+                ["2", "1", "2", "1", "2", "1", "2"],
+                Score(player1=Point(3), player2=Point(4)),
+            ],
         ]
 
         for game, expected in test_cases:
             dashboard = Dashboard(game)
-            dashboard.update()
-            self.assertEqual(dashboard.scores, expected)
+            actual = dashboard.update()
+            self.assertEqual(actual.player1, expected.player1)
+            self.assertEqual(actual.player2, expected.player2)
 
 
 class TestUmpire(TestCase):
     def test_find_winner(self):
         test_cases = [
-            [{"1": 4, "2": 1}, "1"],
-            [{"1": 3, "2": 5}, "2"],
-            [{"1": 6, "2": 4}, "1"],
-            [{"1": 3, "2": 1}, "invalid game"],
-            [{"1": 6, "2": 5}, "invalid game"],
+            [Score(player1=Point(4), player2=Point(1)), "Player 1"],
+            [Score(player1=Point(0), player2=Point(4)), "Player 2"],
+            [Score(player1=Point(3), player2=Point(2)), "invalid game"],
+            [Score(player1=Point(1), player2=Point(2)), "invalid game"],
         ]
 
         umpire = Umpire()
         for scores, expected in test_cases:
-            actual = umpire.find_winner(scores)
+            actual = umpire.find_winner(scores, ["Player 1", "Player 2"])
             self.assertEqual(actual, expected)
 
 
@@ -36,10 +67,10 @@ class TestTennisGame(TestCase):
     def test_run(self):
         test_cases = [
             [["1", "1", "1", "2", "1"], "1"],
-            [["2", "1", "1", "2", "2", "1", "2", "2"], "2"],
-            [["2", "1", "1", "2", "2", "1", "2", "1", "1", "1"], "1"],
+            [["2", "1", "1", "2", "2", "2"], "2"],
+            [["2", "1", "1", "2", "1", "1"], "1"],
             [["a", "b", "a", "a"], "invalid game"],
-            [["2", "1", "1", "2", "2", "1", "2", "1", "1"], "invalid game"],
+            [["2", "1", "1", "2", "2", "1", "2"], "invalid game"],
         ]
 
         for game, expected in test_cases:
