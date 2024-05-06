60295a9e2e374d4358eb2b825cc860bfa694ba4f
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-05-06 16:36:02 +0200

    Returning Fizz for multiples of 3

--- tests/fizzbuzz/test_app.py
+++ tests/fizzbuzz/test_app.py
@@ -6,13 +6,17 @@ from parameterized import parameterized
 class FizzBuzzApp:
 
     def run(self, input:int):
-        return "Fizz"
+        if input % 3 == 0:
+            return "Fizz"
+        return input
 
 
 
 class TestFizzBuzzApp(unittest.TestCase):
 
     @parameterized.expand([
+        [1, 1],
+        [2, 2],
         [3, "Fizz"],
     ])
     def test_run(self, input, expected):
