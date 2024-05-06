10b58135597379030be5bd02447a0c2de10b46f1
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-05-06 16:50:06 +0200

    Improving types

--- src/fizzbuzz/model.py
+++ src/fizzbuzz/model.py
@@ -2,11 +2,11 @@ class Input:
     def __init__(self, value):
         self.value = value
 
-    def is_multiple_of_three(self):
+    def is_multiple_of_three(self) -> bool:
         return self.value % 3 == 0
 
-    def is_multiple_of_five(self):
+    def is_multiple_of_five(self) -> bool:
         return self.value % 5 == 0
 
-    def is_multiple_of_three_and_five(self):
+    def is_multiple_of_three_and_five(self) -> bool:
         return (self.value % 3 == 0) & (self.value % 5 == 0)
