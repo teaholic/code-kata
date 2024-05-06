9ac8c3bf0fa009b330c027343a3c9e3303d1039c
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-05-06 16:37:07 +0200

    Returning Buzz for multiples of 5

--- tests/fizzbuzz/test_app.py
+++ tests/fizzbuzz/test_app.py
@@ -8,6 +8,8 @@ class FizzBuzzApp:
     def run(self, input:int):
         if input % 3 == 0:
             return "Fizz"
+        elif input % 5 == 0:
+            return "Buzz"
         return input
 
 
@@ -18,6 +20,8 @@ class TestFizzBuzzApp(unittest.TestCase):
         [1, 1],
         [2, 2],
         [3, "Fizz"],
+        [4, 4],
+        [5, "Buzz"]
     ])
     def test_run(self, input, expected):
         actual = FizzBuzzApp().run(input)
