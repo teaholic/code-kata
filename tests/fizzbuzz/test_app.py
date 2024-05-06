779b90b35178b5bd760d0f7519e8df768bc3b8da
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-05-06 16:46:52 +0200

    Refactoring

--- tests/fizzbuzz/test_app.py
+++ tests/fizzbuzz/test_app.py
@@ -2,17 +2,31 @@ import unittest
 
 from parameterized import parameterized
 
+class Input:
+    def __init__(self, value):
+        self.value = value
+
+    def is_multiple_of_three(self):
+        return self.value % 3 == 0
+
+    def is_multiple_of_five(self):
+        return self.value % 5 == 0
+
+    def is_multiple_of_three_and_five(self):
+        return (self.value % 3 == 0) & (self.value % 5 == 0)
+
 
 class FizzBuzzApp:
 
-    def run(self, input:int):
-        if (input % 3 == 0) & (input % 5 == 0):
+    def run(self, value:int):
+        my_input = Input(value)
+        if my_input.is_multiple_of_three_and_five():
             return "FizzBuzz"
-        elif input % 3 == 0:
+        elif my_input.is_multiple_of_three():
             return "Fizz"
-        elif input % 5 == 0:
+        elif my_input.is_multiple_of_five():
             return "Buzz"
-        return input
+        return value
 
 
 
