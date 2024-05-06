72ae0a0655c1d68ca2c90ff3279b53e91173d24a
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-05-06 16:41:11 +0200

    Returning FizzBuzz

--- tests/fizzbuzz/test_app.py
+++ tests/fizzbuzz/test_app.py
@@ -6,7 +6,9 @@ from parameterized import parameterized
 class FizzBuzzApp:
 
     def run(self, input:int):
-        if input % 3 == 0:
+        if (input % 3 == 0) & (input % 5 == 0):
+            return "FizzBuzz"
+        elif input % 3 == 0:
             return "Fizz"
         elif input % 5 == 0:
             return "Buzz"
@@ -27,6 +29,11 @@ class TestFizzBuzzApp(unittest.TestCase):
         [8, 8],
         [9, "Fizz"],
         [10, "Buzz"],
+        [11, 11],
+        [12, "Fizz"],
+        [13, 13],
+        [14, 14],
+        [15, "FizzBuzz"],
     ])
     def test_run(self, input, expected):
         actual = FizzBuzzApp().run(input)
