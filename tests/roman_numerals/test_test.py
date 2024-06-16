be648d7348d655c15d463aefe3699e0e8d2fff44
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-06-16 14:17:33 +0200

    New kata util 100, before refactoring

--- tests/roman_numerals/test_test.py
+++ tests/roman_numerals/test_test.py
@@ -1,5 +1,6 @@
 import unittest
 from enum import Enum
+from typing import List
 
 from parameterized import parameterized
 
@@ -19,18 +20,19 @@ class NumeralMappingService:
 
 
 class NumeralService:
-    def __init__(self, number: int):
+    def __init__(self, number: int, numeral_sequence: List[int]):
         self.number = number
+        self.numeral_sequence = numeral_sequence
         self.residues = [self.number - n.value for n in NumeralMapping]
 
     def get_closest_smaller_numeral_number(self) -> int:
         closest_smaller_residue = min([n for n in self.residues if n >= 0])
         closest_smaller_residue_index = self.residues.index(closest_smaller_residue)
-        return [num for pos, num in enumerate([n.value for n in NumeralMapping]) if pos == closest_smaller_residue_index][0]
+        return [num for pos, num in enumerate([n for n in self.numeral_sequence]) if pos == closest_smaller_residue_index][0]
 
     def get_closest_greater_numeral_number(self) -> int:
         closest_greater_residue_index = self.residues.index(self.get_closest_greater_number_residue())
-        return [num for pos, num in enumerate([n.value for n in NumeralMapping]) if pos == closest_greater_residue_index][0]
+        return [num for pos, num in enumerate([n for n in self.numeral_sequence]) if pos == closest_greater_residue_index][0]
 
     def get_closest_greater_number_residue(self) -> int:
         return max([n for n in self.residues if n <= 0])
@@ -39,7 +41,7 @@ class NumeralService:
         closest_smaller_residue = min([n for n in self.residues if n >= 0])
         closest_smaller_tenth_index = self.residues.index(closest_smaller_residue) if self.residues.index(closest_smaller_residue) % 2 == 0 else self.residues.index(closest_smaller_residue) - 1
         print(closest_smaller_tenth_index)
-        return [num for pos, num in enumerate([n.value for n in NumeralMapping]) if pos == closest_smaller_tenth_index][0]
+        return [num for pos, num in enumerate([n for n in self.numeral_sequence]) if pos == closest_smaller_tenth_index][0]
 
 
 class TestNumeralService(unittest.TestCase):
@@ -54,7 +56,7 @@ class TestNumeralService(unittest.TestCase):
         ]
     )
     def test_get_closest_greater_number_residue(self, number, expected):
-        self.assertEqual(NumeralService(number).get_closest_greater_number_residue(), expected)
+        self.assertEqual(NumeralService(number, [n.value for n in NumeralMapping]).get_closest_greater_number_residue(), expected)
 
     @parameterized.expand(
         [
@@ -66,7 +68,7 @@ class TestNumeralService(unittest.TestCase):
         ]
     )
     def test_get_closest_smaller_numeral_number(self, number, expected):
-        self.assertEqual(NumeralService(number).get_closest_smaller_numeral_number(), expected)
+        self.assertEqual(NumeralService(number, [n.value for n in NumeralMapping]).get_closest_smaller_numeral_number(), expected)
 
     @parameterized.expand(
         [
@@ -78,7 +80,7 @@ class TestNumeralService(unittest.TestCase):
         ]
     )
     def test_get_closest_greater_numeral_number(self, number, expected):
-        self.assertEqual(NumeralService(number).get_closest_greater_numeral_number(), expected)
+        self.assertEqual(NumeralService(number, [n.value for n in NumeralMapping]).get_closest_greater_numeral_number(), expected)
 
     @parameterized.expand(
         [
@@ -101,7 +103,7 @@ class TestNumeralService(unittest.TestCase):
         ]
     )
     def test_get_closest_smaller_tenth_number(self, number, expected):
-        self.assertEqual(NumeralService(number).get_closest_smaller_tenth_number(), expected)
+        self.assertEqual(NumeralService(number, [n.value for n in NumeralMapping]).get_closest_smaller_tenth_number(), expected)
 
 
 class TestNumeralMappingService(unittest.TestCase):
@@ -124,38 +126,34 @@ class NumeralApp:
         self.mapping_service = NumeralMappingService()
 
     def convert(self, number:int) -> str:
-        print(number)
-        closest_greater_number_residue = NumeralService(number).get_closest_greater_number_residue()
-        print("Closest greater residue" + str(closest_greater_number_residue))
+        closest_greater_number_residue = NumeralService(number, [n.value for n in NumeralMapping]).get_closest_greater_number_residue()
         if closest_greater_number_residue == 0:
             return self.mapping_service.get_roman_numeral(number)
         else:
-            closest_smaller_number = NumeralService(number).get_closest_smaller_numeral_number()
-            closest_greater_number = NumeralService(number).get_closest_greater_numeral_number()
-            print("Closest smaller" + str(closest_smaller_number))
-            print("Closest greater" + str(closest_greater_number))
+            closest_smaller_number = NumeralService(number, [n.value for n in NumeralMapping]).get_closest_smaller_numeral_number()
+            closest_greater_number = NumeralService(number, [n.value for n in NumeralMapping]).get_closest_greater_numeral_number()
             if (number % closest_smaller_number == 0) & (closest_greater_number + closest_greater_number_residue != number):#number < 4:
                 return self.mapping_service.get_roman_numeral(closest_smaller_number) * round(number / closest_smaller_number)
             else:
-                if closest_greater_number_residue == - NumeralService(number).get_closest_smaller_tenth_number():
-                    if closest_smaller_number == NumeralService(number).get_closest_smaller_tenth_number():
+                if closest_greater_number_residue == - NumeralService(number, [n.value for n in NumeralMapping]).get_closest_smaller_tenth_number():
+                    if closest_smaller_number == NumeralService(number, [n.value for n in NumeralMapping]).get_closest_smaller_tenth_number():
                         second_closest_smaller_number = NumeralService(
-                            closest_smaller_number).get_closest_smaller_numeral_number()
+                            closest_smaller_number, [n.value for n in NumeralMapping]).get_closest_smaller_numeral_number()
                     else:
                         second_closest_smaller_number = NumeralService(
-                            closest_smaller_number - 1).get_closest_smaller_numeral_number()
+                            closest_smaller_number - 1, [n.value for n in NumeralMapping]).get_closest_smaller_numeral_number()
                     print(second_closest_smaller_number)
-                    closest_greater_number = NumeralService(number).get_closest_greater_numeral_number()
+                    closest_greater_number = NumeralService(number, [n.value for n in NumeralMapping]).get_closest_greater_numeral_number()
                     return self.mapping_service.get_roman_numeral(second_closest_smaller_number) + self.mapping_service.get_roman_numeral(closest_greater_number)
                 else:
-                    print("Corner case")
                     if number % closest_smaller_number == 0:
                         return self.mapping_service.get_roman_numeral(closest_smaller_number) * round(
                             number / closest_smaller_number)
                     else:
                         second_closest_smaller_number = NumeralService(
-                            closest_smaller_number - 1).get_closest_smaller_numeral_number()
-                        roman_numeral_substring = self.mapping_service.get_roman_numeral(second_closest_smaller_number) * (number - closest_smaller_number)
+                            closest_smaller_number - 1, [n.value for n in NumeralMapping]).get_closest_smaller_numeral_number()
+                        print(second_closest_smaller_number)
+                        roman_numeral_substring = self.mapping_service.get_roman_numeral(second_closest_smaller_number) * round((number - closest_smaller_number)/second_closest_smaller_number)
                         return self.mapping_service.get_roman_numeral(closest_smaller_number) + roman_numeral_substring
 
 
@@ -175,7 +173,13 @@ class TestNumeralApp(unittest.TestCase):
             (10, "X"),
             (20, "XX"),
             (30, "XXX"),
-            (40, "XL")
+            (40, "XL"),
+            (50, "L"),
+            (60, "LX"),
+            (70, "LXX"),
+            (80, "LXXX"),
+            (90, "XC"),
+            (100, "C"),
         ]
     )
     def test_convert(self, number, expected):
