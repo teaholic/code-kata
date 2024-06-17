c2ae1c935d2bfce5fd4f251e9df82488793ad6a4
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2024-06-17 19:17:39 +0200

    WIP refactoring

--- src/roman_numerals/app.py
+++ src/roman_numerals/app.py
@@ -1,38 +1,51 @@
 from src.roman_numerals.numeral_gateway import NumeralGateway
-from src.roman_numerals.model import RomanToArabicMappingService, RomanToArabicMapping
+from src.roman_numerals.model import RomanToArabicMappingService
 
 
 class NumeralApp:
-    def __init__(self):
-        self.mapping_service = RomanToArabicMappingService()
+    def convert(self, number: int) -> str:
+        return NumeralConversion(number).run()
 
-    def convert(
-        self, number: int
-    ) -> str:  # Todo: extract and model NumeralGateway calculations
-        closest_greater_number_distance = NumeralGateway(
-            number
-        ).get_closest_greater_distance()
-        if closest_greater_number_distance == 0:
-            return self.mapping_service.get_roman_numeral(number)
+
+class ArabicNumber:
+    def __init__(self, number):
+        self.number = number
+        self.api = NumeralGateway(number)
+
+    def has_one_digit_roman_match(self) -> bool:
+        return self.api.get_closest_greater_distance() == 0
+
+
+class NumeralConversion:
+    def __init__(self, number):
+        self.number = number
+        self.mapping_service = RomanToArabicMappingService()
+        self.api = NumeralGateway(number)
+        self.number_api = ArabicNumber(number)
+
+    def run(self) -> str:
+        closest_greater_number_distance = self.api.get_closest_greater_distance()
+        if (
+            self.number_api.has_one_digit_roman_match()
+        ):  # closest_greater_number_distance == 0:
+            return self.mapping_service.get_roman_numeral(self.number)
         else:
-            closest_smaller_number = NumeralGateway(number).get_closest_smaller_number()
-            closest_greater_number = NumeralGateway(number).get_closest_greater_number()
-            if (number % closest_smaller_number == 0) & (
-                closest_greater_number + closest_greater_number_distance != number
+            closest_smaller_number = self.api.get_closest_smaller_number()
+            closest_greater_number = self.api.get_closest_greater_number()
+            if (self.number % closest_smaller_number == 0) & (
+                closest_greater_number + closest_greater_number_distance != self.number
             ):  # number < 4:
                 return self.mapping_service.get_roman_numeral(
                     closest_smaller_number
-                ) * round(number / closest_smaller_number)
+                ) * round(self.number / closest_smaller_number)
             else:
                 if (
                     closest_greater_number_distance
-                    == -NumeralGateway(number).get_closest_smaller_order_of_magnitude()
+                    == -self.api.get_closest_smaller_order_of_magnitude()
                 ):
                     if (
                         closest_smaller_number
-                        == NumeralGateway(
-                            number
-                        ).get_closest_smaller_order_of_magnitude()
+                        == self.api.get_closest_smaller_order_of_magnitude()
                     ):
                         second_closest_smaller_number = NumeralGateway(
                             closest_smaller_number
@@ -42,17 +55,15 @@ class NumeralApp:
                             closest_smaller_number - 1
                         ).get_closest_smaller_number()
                     print(second_closest_smaller_number)
-                    closest_greater_number = NumeralGateway(
-                        number
-                    ).get_closest_greater_number()
+                    closest_greater_number = self.api.get_closest_greater_number()
                     return self.mapping_service.get_roman_numeral(
                         second_closest_smaller_number
                     ) + self.mapping_service.get_roman_numeral(closest_greater_number)
                 else:
-                    if number % closest_smaller_number == 0:
+                    if self.number % closest_smaller_number == 0:
                         return self.mapping_service.get_roman_numeral(
                             closest_smaller_number
-                        ) * round(number / closest_smaller_number)
+                        ) * round(self.number / closest_smaller_number)
                     else:
                         second_closest_smaller_number = NumeralGateway(
                             closest_smaller_number - 1,
@@ -60,7 +71,7 @@ class NumeralApp:
                         roman_numeral_substring = self.mapping_service.get_roman_numeral(
                             second_closest_smaller_number
                         ) * round(
-                            (number - closest_smaller_number)
+                            (self.number - closest_smaller_number)
                             / second_closest_smaller_number
                         )
                         return (
